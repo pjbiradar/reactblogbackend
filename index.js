43b12366ec44a541c9e8955e950a2b35a0181f3b
pjbiradar <poojabiradar2412@gmail.com>
2022-02-17 20:03:19 +0530

    test

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const reactblogroute=require('./router/reactblogroute')
 const cors=require("cors")
 app.use(cors())
 
+const PORT = process.env.PORT || 8000;
+
+
 app.use("/api/v1",reactblogroute)
-app.listen(8000,()=>{
+app.listen(PORT,()=>{
     console.log("application successed");
 })
